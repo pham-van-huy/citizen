0d4d4bfbac47b0ab08a1e252824d05ae52e325a5
Lukáš Lukáč <lukas.lukac.io@gmail.com>
2018-10-12 17:35:04 +0200

    #5 Adds Apache license link to each file header.

--- httpserver/req_res_types.go
+++ httpserver/req_res_types.go
@@ -1,3 +1,6 @@
+// Copyright 2018 https://gophersland.com
+// All rights reserved.
+// Use of this source code is governed by an Apache License that can be found in the LICENSE file.
 package httpserver
 
 type pingReq struct {
